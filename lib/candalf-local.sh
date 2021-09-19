6f9aaa1b7e3c910587c56431f93f6dc9a1ff02e4
Jarmo Pertman <jarmo@jarmopertman.com>
2021-09-19 14:02:32 +0300

    Fix applying spells locally

--- lib/candalf-local.sh
+++ lib/candalf-local.sh
@@ -4,23 +4,28 @@ VERBOSE="${VERBOSE:-""}"
 test "$VERBOSE" && set -x
 set -Eeuo pipefail
 
-CANDALF_REMOTE_ROOT=$HOME/.candalf
+CANDALF_REMOTE_ROOT="$HOME/.candalf"
 
 . "$CANDALF_ROOT"/lib/candalf-env.sh
 eval "$(candalfEnv)"
 
 candalf() {
   SPELL_BOOK="${2:?"SPELL_BOOK not set!"}"
+  SPELL_BOOK_PATH="$(realpath "$SPELL_BOOK")"
+  SPELL_BOOK_DIR="$(dirname "$SPELL_BOOK_PATH")"
+  SPELL_BOOK_BASENAME="$(basename "$SPELL_BOOK")"
+  SPELL_BOOK_BASENAME_WITHOUT_EXT="$(basename "$SPELL_BOOK_BASENAME" .sh)"
+  CANDALF_SPELLS_ROOT="$CANDALF_REMOTE_ROOT/$SPELL_BOOK_BASENAME_WITHOUT_EXT"
 
-  rsync -ac "$CANDALF_ROOT"/lib/cast.sh "$CANDALF_REMOTE_ROOT"/lib/cast.sh
+  rsync -ac "$CANDALF_ROOT"/lib/cast.sh "$CANDALF_ROOT/lib/candalf-env.sh" "$CANDALF_REMOTE_ROOT"/lib
 
-  rsync -Rac "$SPELL_BOOK" \
-    "$(grep -E "^cast.*\.sh" "$SPELL_BOOK" | rev | awk '{print $1}' | rev)" \
-    "$CANDALF_REMOTE_ROOT"
+  cd "$SPELL_BOOK_DIR"
+  rsync --exclude ".**" -Rac "." "$CANDALF_SPELLS_ROOT"
+  cd - >/dev/null
 
-    # shellcheck disable=SC2154
-    env "${candalfEnvVars[@]-}" bash -c "CANDALF_ROOT=$CANDALF_REMOTE_ROOT VERBOSE=$VERBOSE \
-      $CANDALF_REMOTE_ROOT/$SPELL_BOOK 2>&1" | tee -a /var/log/candalf.log
+  # shellcheck disable=SC2154
+  env CANDALF_ROOT="$CANDALF_REMOTE_ROOT" CANDALF_SPELLS_ROOT="$CANDALF_SPELLS_ROOT" VERBOSE="$VERBOSE" "${candalfEnvVars[@]-}" \
+    bash -c "$CANDALF_SPELLS_ROOT/$SPELL_BOOK_BASENAME" 2>&1 | tee -a /var/log/candalf.log
 }
 
 bootstrap() {
